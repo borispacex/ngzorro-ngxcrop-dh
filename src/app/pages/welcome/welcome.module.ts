ed6d2b13ab5428ac091986b5f422dfc6930559b4
Boris Vargas <borisvargaspaucara@gmail.com>
2025-04-26 09:36:26 -0400

    deploy to docs

--- src/app/pages/welcome/welcome.module.ts
+++ src/app/pages/welcome/welcome.module.ts
@@ -4,17 +4,10 @@ import { WelcomeRoutingModule } from './welcome-routing.module';
 
 import { WelcomeComponent } from './welcome.component';
 import {ImageCropperModule} from "ngx-image-cropper";
-import {NzButtonModule} from "ng-zorro-antd/button";
-import {NzIconModule} from "ng-zorro-antd/icon";
-import {NzUploadModule} from "ng-zorro-antd/upload";
-
 
 @NgModule({
   imports: [
     WelcomeRoutingModule,
-    NzButtonModule,
-    NzIconModule,
-    NzUploadModule,
     ImageCropperModule
   ],
   declarations: [WelcomeComponent],
